763d063e4144a5d4ac2fd060f9fc0aba1d2092c1
Coatl <121911012+2-Coatl@users.noreply.github.com>
2025-10-28 08:19:48 -0600

    test(project): asegurar configuracion callcentersite

---
+++ callcentersite/wsgi.py
@@ -0,0 +1,9 @@
+"""Configuración WSGI para el proyecto Call Center Analytics."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'callcentersite.settings')
+
+application = get_wsgi_application()
