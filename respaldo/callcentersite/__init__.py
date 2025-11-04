c053d4a23cc96503119c1434f5358d0496159392
Coatl <121911012+2-Coatl@users.noreply.github.com>
2025-11-03 23:03:54 -0600

    Feat: update project

--- respaldo/callcentersite/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-CallCenter Site - API Django Application
-"""
-
-__version__ = "0.1.0"
-__author__ = "CallCenter Team"
--- respaldo/callcentersite/callcentersite/wsgi.py
+++
@@ -1,9 +0,0 @@
-"""Configuración WSGI para despliegue con Apache + mod_wsgi."""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "callcentersite.settings.production")
-
-application = get_wsgi_application()
